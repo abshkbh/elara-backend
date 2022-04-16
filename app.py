2869669abe016739617b658d0a6fa6582401a1fc
Abhishek Bhardwaj <abshkbh@gmail.com>
2022-04-16 15:05:24 -0700

    Add comments

--- app.py
+++ app.py
@@ -11,26 +11,45 @@ from flask_login import current_user, login_required, login_user, UserMixin
 from flask_login import LoginManager
 from werkzeug.security import generate_password_hash, check_password_hash
 
+# URL to hit to verify a Google OAuth token.
 GOOGLE_OAUTH_TOKEN_VERIFICATION_URL = "https://oauth2.googleapis.com/tokeninfo"
 
 app = Flask(__name__)
+
+# We will use sessions / cookies. This requires |support_credentials| to be True.
 CORS(app, supports_credentials=True)
+
 app.config['MONGODB_SETTINGS'] = {
+    # TODO: Is this needed if we are using a cloud instance.
     'db': 'your_database',
+    # Mongo DB Instance in the cloud.
     'host': 'mongodb+srv://elaraadmin:test0000@cluster0.kh4nl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
+
+# Required for cookie signing.
 app.config['SECRET_KEY'] = '1a2b9bdd22abaed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcc0'
+
+# Required so that cookies are persisted by the Browser (especially Chrome) on cross origin clients.
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'
 app.config['SESSION_COOKIE_SECURE'] = True
+
 db = MongoEngine(app)
 login = LoginManager(app)
 
 
+# Adds headers to an API response needed -
+# To be able to process API calls from cross origin clients.
+# To tell clients that we support cookies.
 def response_with_cors(response, request):
     response.headers.add('Access-Control-Allow-Origin',
                          request.environ.get('HTTP_ORIGIN', '*'))
     response.headers.add('Access-Control-Allow-Credentials', 'true')
     return response
+
+
+"""
+Each Annotation is a time stamp and string contents.
+"""
 
 
 class Annotation(db.EmbeddedDocument):
@@ -44,12 +63,24 @@ class Annotation(db.EmbeddedDocument):
         }
 
 
+"""
+The main object associated with a user. This will be stored in our database.
+"""
+
+
 class User(UserMixin, db.Document):
+    # Auto incrementing id associatef with each object.
     uid = db.ObjectIdField(default=bson.ObjectId, primary_key=True)
+
+    # Email. Always required.
     email = db.StringField()
+
+    # Password hash associated with a user's password. It can be an empty string if the user has logged in via the OAuth flow.
     password_hash = db.StringField()
+
     # Maps YT video id => [{"time_stamp": XX, "content": YY}, ...].
     annotations = db.MapField(db.EmbeddedDocumentListField(Annotation))
+
     # Maps YT video id to its title. E.g. "abcd34sq" => "Top 10 moments of 2022".
     video_id_title_map = db.MapField(db.StringField())
 
@@ -68,9 +99,9 @@ class User(UserMixin, db.Document):
         return check_password_hash(self.password_hash, password)
 
 
+# Used with flask-login to authenticate a User.
 @login.user_loader
 def load_user(id):
-    print("Loader id={}".format(id))
     return User.objects(uid=id).first()
 
 
@@ -83,6 +114,22 @@ if not user:
     user.set_password("foo1234!")
     user.save()
 
+"""
+This API is used to login a user via Google OAuth.
+
+This API expects -
+{
+    'token' : <token>,
+}
+in the POST data.
+
+It verifies this token via hitting the Google OAuth API. If the verification succeeds -
+- If the user is new it creates a new user.
+- If the user exists, it retrieves the existing user.
+
+It then logs in the associated user and returns 200. Else returns a 400 error.
+"""
+
 
 @app.route("/v1/oauth/google/login", methods=['POST'])
 def oauth_google_login():
@@ -91,12 +138,12 @@ def oauth_google_login():
     token_verification_response = requests.get(
         GOOGLE_OAUTH_TOKEN_VERIFICATION_URL, {'id_token': token})
     if token_verification_response.status_code != requests.codes.ok:
-        print("Error: Google Oauth token not verified")
+        print("Error: Google OAuth token not verified")
         return response_with_cors(make_response(jsonify({'error': 'google oauth token not verified'}), 400), request)
 
     token_verification_response_json = token_verification_response.json()
     if not token_verification_response_json["email_verified"]:
-        print("Error: Google Oauth email not verified")
+        print("Error: Google OAuth email not verified")
         return response_with_cors(make_response(jsonify({'error': 'google oauth email not verified'}), 400), request)
 
     email = token_verification_response_json["email"]
@@ -107,9 +154,25 @@ def oauth_google_login():
                     annotations={}, video_id_title_map={})
         user.save()
 
-    print("Login user with email: {} after Oauth", email)
+    print("Login user with email: {} after OAuth", email)
     login_user(user)
     return response_with_cors(jsonify({'msg': 'success'}), request)
+
+
+"""
+This API is used to login a user via user name and password.
+
+This API expects -
+{
+    'email' : <email>,
+    'password': <password>,
+}
+in the POST data.
+
+It tries to retrieve the user corresponding to the email and password.
+- If the user exists it logs in the user and returns 200.
+- If no user exists it returns a 400 error.
+"""
 
 
 @app.route('/v1/login', methods=['POST'])
@@ -138,10 +201,25 @@ def login():
     return response_with_cors(jsonify(user.to_json()), request)
 
 
+"""
+This API returns the list of videos for the current authenticated user.
+"""
+
+
 @app.route('/v1/list', methods=['GET'])
 @login_required
 def query_records():
     return response_with_cors(jsonify(user_videos=current_user.video_id_title_map), request)
+
+
+"""
+This API returns the annotations corresponding to a video for the current authenticated user.
+
+This API expects "video_id=<video_id>" as a query parameter.
+
+For e.g. for this Youtube URL https://www.youtube.com/watch?v=TcAAARgLZ8M the video id will be
+"TcAAARgLZ8M".
+"""
 
 
 @app.route('/v1/annotations', methods=['GET'])
@@ -152,6 +230,17 @@ def query_annotations():
         print("Error: Video Id empty")
         return response_with_cors(make_response(jsonify({'error': 'video id empty'}), 400), request)
     return response_with_cors(jsonify(annotations=current_user.annotations[video_id]), request)
+
+
+"""
+This API adds an annotation associated with a video. It expects -
+{
+    'video_id' : <video_id>,
+    'ts': <time_stamp>,
+    'content': <content>,
+    'video_title': <video_title>,
+} in the POST data.
+"""
 
 
 @app.route('/v1/add', methods=['PUT'])
